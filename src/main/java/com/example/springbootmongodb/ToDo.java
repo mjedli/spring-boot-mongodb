b7ede9659f63d21e8bdacc64b6b516b0246eed9d
mjedli <jedli.mejdi.recup@gmail.com>
2020-06-21 14:44:14 +0200

    restcontroller

---
+++ src/main/java/com/example/springbootmongodb/ToDo.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.example.springbootmongodb;
+
+/**
+ * @author mjedli
+ *
+ */
+public class ToDo {
+	
+	private int id;
+	private String name;
+	private String description;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
---
+++ src/main/java/com/example/springbootmongodb/ToDoController.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.example.springbootmongodb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mjedli
+ *
+ */
+@RestController
+public class ToDoController {
+
+	static List<ToDo> data;
+	
+	static {
+	ToDo todo1 = new ToDo();
+	
+	todo1.setId(1);
+	todo1.setName("name1");
+	todo1.setDescription("Desciption of todo");
+	
+	
+	ToDo todo2 = new ToDo();
+	
+	todo2.setId(2);
+	todo2.setName("name2");
+	todo2.setDescription("Desciption of todo");
+	
+	 data = new ArrayList<>(java.util.Arrays.asList(todo1,todo2));
+	
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ToDo getToDo(@PathVariable int id) {
+		
+		ToDo todo = new ToDo();
+		
+		todo.setId(id);
+		todo.setName("name");
+		todo.setDescription("Desciption of todo");
+		
+		return todo ;
+	}
+	
+	@GetMapping(value = "/")
+	public List<ToDo> getAll() {
+		return data;
+	}
+	
+}
