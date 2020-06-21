d2f28b56f3ef8e5074bffd2a980ee5afd6647cc1
mjedli <jedli.mejdi.recup@gmail.com>
2020-06-21 21:48:15 +0200

    restcontroller

--- src/main/java/com/example/springbootmongodb/ToDoController.java
+++ src/main/java/com/example/springbootmongodb/ToDoController.java
@@ -6,15 +6,11 @@ package com.example.springbootmongodb;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-import javax.websocket.server.PathParam;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -27,20 +23,20 @@ public class ToDoController {
 	static List<ToDo> data;
 	
 	static {
-	ToDo todo1 = new ToDo();
-	
-	todo1.setId(1);
-	todo1.setName("name1");
-	todo1.setDescription("Desciption of todo");
-	
-	
-	ToDo todo2 = new ToDo();
-	
-	todo2.setId(2);
-	todo2.setName("name2");
-	todo2.setDescription("Desciption of todo");
-	
-	 data = new ArrayList<>(java.util.Arrays.asList(todo1,todo2));
+		ToDo todo1 = new ToDo();
+		
+		todo1.setId(1);
+		todo1.setName("name1");
+		todo1.setDescription("Desciption of todo");
+		
+		
+		ToDo todo2 = new ToDo();
+		
+		todo2.setId(2);
+		todo2.setName("name2");
+		todo2.setDescription("Desciption of todo");
+		
+		 data = new ArrayList<>(java.util.Arrays.asList(todo1,todo2));
 	
 	}
 	
@@ -68,4 +64,9 @@ public class ToDoController {
 		return toDo;
 	}
 	
+	@DeleteMapping(value= {"/{id}"})
+	public List<ToDo> removeTodoById(@PathVariable int id) {
+		data.remove(id);
+		return data;
+	}
 }
